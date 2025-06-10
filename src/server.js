1b3bf86010305c03426558b9105305713ae64c77
edward <edwardlfish@gmail.com>
2025-06-10 21:16:29 +0300

    Basic Auth complete

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ import postRoutes from "./routes/post.routes.js";
 import errorHandler from "./middleware/error.middleware.js";
 import config from "./config/config.js";
 import userAccountRoutes from "./routes/userAccount.routes.js";
+import authentication from "./middleware/authentication.middleware.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(authentication);
 app.use('/forum', postRoutes);
 app.use('/account', userAccountRoutes);
 app.use(errorHandler);
